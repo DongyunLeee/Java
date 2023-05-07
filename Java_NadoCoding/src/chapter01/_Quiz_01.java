0fb7b02c3a8af359d54af002d94c22ada1fd78bc
DongyunLeee <speed1252@gmail.com>
2023-05-08 01:35:24 +0900

    [chapter01] Quiz

---
+++ Java_NadoCoding/src/chapter01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNo = "상암08"; // 버스 번호
+        int minutes = 5; // 버스 도착 예정 시간
+        float distance = 1.5F; // 남은 거리
+
+        System.out.println(busNo + " 버스");
+        System.out.println("약 " + minutes + "분 후 도착 예정");
+        System.out.println("남은 거리 " + distance + "km");
+    }
+}
